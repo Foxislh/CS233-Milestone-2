b0622d89e5293cbf830ca34881d4cf2d749c0673
taishuix <ts.yibing.wan@gmail.com>
2024-05-28 21:22:16 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,153 @@
+import argparse
+
+import numpy as np
+from torchinfo import summary
+
+from src.data import load_data
+from src.methods.pca import PCA
+from src.methods.dummy_methods import DummyClassifier
+from src.methods.deep_network import MLP, CNN, Trainer, MyViT
+from src.utils import normalize_fn, append_bias_term, accuracy_fn, macrof1_fn, get_n_classes
+
+
+def main(args):
+    """
+    The main function of the script. Do not hesitate to play with it
+    and add your own code, visualization, prints, etc!
+
+    Arguments:
+        args (Namespace): arguments that were parsed from the command line (see at the end 
+                          of this file). Their value can be accessed as "args.argument".
+    """
+    ## 1. First, we load our data and flatten the images into vectors
+    xtrain, xtest, ytrain = load_data(args.data)
+    xtrain = xtrain.reshape(xtrain.shape[0], -1)
+    xtest = xtest.reshape(xtest.shape[0], -1)
+
+    ## 2. Then we must prepare it. This is were you can create a validation set,
+    #  normalize, add bias, etc.
+
+    # Make a validation set
+    if not args.test:
+    ### WRITE YOUR CODE HERE
+        N = xtrain.shape[0]  
+        val_ratio = 0.2
+
+        val_size = int(N * val_ratio)  
+        indices = np.arange(N)
+        np.random.shuffle(indices)
+
+        # Split index into training set and verification set
+        train_indices = indices[val_size:]
+        val_indices = indices[:val_size]
+
+        xtest = xtrain[val_indices]
+        ytest = ytrain[val_indices]
+
+        xtrain = xtrain[train_indices]
+        ytrain = ytrain[train_indices]
+
+        pass
+
+    ### WRITE YOUR CODE HERE to do any other data processing
+    mean_xtrain = np.mean(xtrain, axis = 0, keepdims = True)
+    std_xtrain = np.std(xtrain, axis = 0, keepdims = True)
+
+    mean_xtest = np.mean(xtest, axis = 0, keepdims = True)
+    std_xtest = np.std(xtest, axis = 0, keepdims = True)
+
+    # normalization
+    xtrain = normalize_fn(xtrain, mean_xtrain, std_xtrain)
+    xtest = normalize_fn(xtest, mean_xtest, std_xtest)
+
+    # append bias term
+    xtrain = append_bias_term(xtrain)
+    xtest = append_bias_term(xtest) 
+
+    # flatten input
+    xtrain = xtrain.reshape(xtrain.shape[0], -1)  
+    xtest = xtest.reshape(xtest.shape[0], -1)  
+
+    # Dimensionality reduction (MS2)
+    if args.use_pca:
+        print("Using PCA")
+        pca_obj = PCA(d=args.pca_d)
+        ### WRITE YOUR CODE HERE: use the PCA object to reduce the dimensionality of the data
+        pca_obj.find_principal_components(xtrain)
+        xtrain = pca_obj.reduce_dimension(xtrain)
+        xtest = pca_obj.reduce_dimension(xtest)
+
+    ## 3. Initialize the method you want to use.
+
+    # Neural Networks (MS2)
+
+    # Prepare the model (and data) for Pytorch
+    # Note: you might need to reshape the data depending on the network you use!
+    input_size = xtrain.shape[1]
+    n_classes = get_n_classes(ytrain)
+    if args.nn_type == "mlp":
+        model = MLP(input_size, n_classes) ### WRITE YOUR CODE HERE
+    elif args.nn_type == "cnn":
+        model = CNN(input_size, n_classes)
+    elif args.nn_type == "transformer":
+        model = MyViT(chw = input_size, out_d = n_classes)
+    else:
+        pass
+
+    summary(model)
+
+    # Trainer object
+    method_obj = Trainer(model, lr=args.lr, epochs=args.max_iters, batch_size=args.nn_batch_size)
+
+    ## 4. Train and evaluate the method
+
+    # Fit (:=train) the method on the training data
+    preds_train = method_obj.fit(xtrain, ytrain)
+
+    # Predict on unseen data
+    preds = method_obj.predict(xtest)
+
+    ## Report results: performance on train and valid/test sets
+    acc = accuracy_fn(preds_train, ytrain)
+    macrof1 = macrof1_fn(preds_train, ytrain)
+    print(f"\nTrain set: accuracy = {acc:.3f}% - F1-score = {macrof1:.6f}")
+
+    if not args.test:
+    ## As there are no test dataset labels, check your model accuracy on validation dataset.
+    # You can check your model performance on test set by submitting your test set predictions on the AIcrowd competition.
+        acc = accuracy_fn(preds, ytest)
+        macrof1 = macrof1_fn(preds, ytest)
+        print(f"Validation set:  accuracy = {acc:.3f}% - F1-score = {macrof1:.6f}")
+
+        pass
+
+    ### WRITE YOUR CODE HERE if you want to add other outputs, visualization, etc.
+
+
+if __name__ == '__main__':
+    # Definition of the arguments that can be given through the command line (terminal).
+    # If an argument is not given, it will take its default value as defined below.
+    parser = argparse.ArgumentParser()
+    # Feel free to add more arguments here if you need!
+
+    # MS2 arguments
+    parser.add_argument('--data', default="dataset", type=str, help="path to your dataset")
+    parser.add_argument('--nn_type', default="mlp",
+                        help="which network architecture to use, it can be 'mlp' | 'transformer' | 'cnn'")
+    parser.add_argument('--nn_batch_size', type=int, default=64, help="batch size for NN training")
+    parser.add_argument('--device', type=str, default="cpu",
+                        help="Device to use for the training, it can be 'cpu' | 'cuda' | 'mps'")
+    parser.add_argument('--use_pca', action="store_true", help="use PCA for feature reduction")
+    parser.add_argument('--pca_d', type=int, default=100, help="the number of principal components")
+
+
+    parser.add_argument('--lr', type=float, default=1e-5, help="learning rate for methods with learning rate")
+    parser.add_argument('--max_iters', type=int, default=100, help="max iters for methods which are iterative")
+    parser.add_argument('--test', action="store_true",
+                        help="train on whole training data and evaluate on the test data, otherwise use a validation set")
+
+
+    # "args" will keep in memory the arguments and their values,
+    # which can be accessed as "args.data", for example.
+    args = parser.parse_args()
+    main(args)
---
+++ src/methods/deep_network.py
@@ -0,0 +1,309 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.utils.data import TensorDataset, DataLoader
+
+## MS2
+
+
+class MLP(nn.Module):
+    """
+    An MLP network which does classification.
+
+    It should not use any convolutional layers.
+    """
+
+    def __init__(self, input_size, n_classes):
+        """
+        Initialize the network.
+        
+        You can add arguments if you want, but WITH a default value, e.g.:
+            __init__(self, input_size, n_classes, my_arg=32)
+        
+        Arguments:
+            input_size (int): size of the input
+            n_classes (int): number of classes to predict
+        """
+        super().__init__()
+        ##
+        ###
+        #### WRITE YOUR CODE HERE!
+        ###
+        ##
+
+        self.fc1 = nn.Linear(in_features=input_size, out_features=64, bias=True)
+        self.fc2 = nn.Linear(in_features=64, out_features=128, bias=True)
+        self.fc3 = nn.Linear(in_features=128, out_features=256, bias=True)
+        self.fc4 = nn.Linear(in_features=256, out_features=n_classes, bias=True)
+
+    def forward(self, x):
+        """
+        Predict the class of a batch of samples with the model.
+
+        Arguments:
+            x (tensor): input batch of shape (N, D)
+        Returns:
+            preds (tensor): logits of predictions of shape (N, C)
+                Reminder: logits are value pre-softmax.
+        """
+        ##
+        ###
+        #### WRITE YOUR CODE HERE!
+        ###
+        ##
+
+        x = F.relu(self.fc1(x))  
+        x = F.relu(self.fc2(x)) 
+        x = F.relu(self.fc3(x)) 
+        preds = self.fc4(x)      
+        return preds
+
+
+class CNN(nn.Module):
+    """
+    A CNN which does classification.
+
+    It should use at least one convolutional layer.
+    """
+
+    def __init__(self, input_channels, n_classes):
+        """
+        Initialize the network.
+        
+        You can add arguments if you want, but WITH a default value, e.g.:
+            __init__(self, input_channels, n_classes, my_arg=32)
+        
+        Arguments:
+            input_channels (int): number of channels in the input
+            n_classes (int): number of classes to predict
+        """
+        super().__init__()
+        ##
+        ###
+        #### WRITE YOUR CODE HERE!
+        ###
+        ##
+        self.conv_block1 = nn.Sequential(
+            nn.Conv1d(1, 32, kernel_size=3, padding=1),
+            nn.ReLU(inplace = True),
+            nn.Conv1d(32, 32, kernel_size=3, padding=1), 
+            nn.ReLU(inplace = True),
+            nn.MaxPool1d(kernel_size=2, stride=2)
+        )
+        self.conv_block2 = nn.Sequential(
+            nn.Conv1d(32, 64, kernel_size=3, padding=1),
+            nn.ReLU(inplace = True),
+            nn.Conv1d(64, 64, kernel_size=3, padding=1), 
+            nn.ReLU(inplace = True),
+            nn.MaxPool1d(kernel_size=2, stride=2)
+        )
+        self.conv_block3 = nn.Sequential(
+            nn.Conv1d(64, 128, kernel_size=3, padding=1),
+            nn.ReLU(inplace = True),
+            nn.Conv1d(128, 128, kernel_size=3, padding=1), 
+            nn.ReLU(inplace = True),
+            nn.Conv1d(128, 128, kernel_size=3, padding=1), 
+            nn.ReLU(inplace = True),
+            nn.MaxPool1d(kernel_size=2, stride=2)
+        )
+        reduced_size = input_channels // 4 
+        self.fc1 = nn.Linear(64 * reduced_size, 512)
+        self.fc2 = nn.Linear(512, n_classes)
+
+    def forward(self, x):
+        """
+        Predict the class of a batch of samples with the model.
+
+        Arguments:
+            x (tensor): input batch of shape (N, Ch, H, W)
+        Returns:
+            preds (tensor): logits of predictions of shape (N, C)
+                Reminder: logits are value pre-softmax.
+        """
+        ##
+        ###
+        #### WRITE YOUR CODE HERE!
+        ###
+        ##
+        x = x.unsqueeze(1) # add the channel
+        x = self.conv_block1(x)
+        x = self.conv_block2(x)
+        x = x.view(x.size(0), -1)  # Flatten
+        x = F.relu(self.fc1(x))
+        preds = self.fc2(x)
+        return preds
+
+
+class MyViT(nn.Module):
+    """
+    A Transformer-based neural network
+    """
+
+    def __init__(self, chw, n_patches=1, n_blocks=1, hidden_d=1, n_heads=1, out_d=1):
+        """
+        Initialize the network.
+        
+        """
+        super().__init__()
+        ##
+        ###
+        #### WRITE YOUR CODE HERE!
+        ###
+        ##
+
+    def forward(self, x):
+        """
+        Predict the class of a batch of samples with the model.
+
+        Arguments:
+            x (tensor): input batch of shape (N, Ch, H, W)
+        Returns:
+            preds (tensor): logits of predictions of shape (N, C)
+                Reminder: logits are value pre-softmax.
+        """
+        ##
+        ###
+        #### WRITE YOUR CODE HERE!
+        ###
+        ##
+        return preds
+
+
+class Trainer(object):
+    """
+    Trainer class for the deep networks.
+
+    It will also serve as an interface between numpy and pytorch.
+    """
+
+    def __init__(self, model, lr, epochs, batch_size):
+        """
+        Initialize the trainer object for a given model.
+
+        Arguments:
+            model (nn.Module): the model to train
+            lr (float): learning rate for the optimizer
+            epochs (int): number of epochs of training
+            batch_size (int): number of data points in each batch
+        """
+        self.lr = lr
+        self.epochs = epochs
+        self.model = model
+        self.batch_size = batch_size
+
+        self.criterion = nn.CrossEntropyLoss()
+        self.optimizer = torch.optim.SGD(model.parameters(), lr=lr) ### WRITE YOUR CODE HERE
+
+    def train_all(self, dataloader):
+        """
+        Fully train the model over the epochs. 
+        
+        In each epoch, it calls the functions "train_one_epoch". If you want to
+        add something else at each epoch, you can do it here.
+
+        Arguments:
+            dataloader (DataLoader): dataloader for training data
+        """
+        for ep in range(self.epochs):
+            self.train_one_epoch(dataloader, ep)
+
+            ### WRITE YOUR CODE HERE if you want to do add something else at each epoch
+
+    def train_one_epoch(self, dataloader, ep):
+        """
+        Train the model for ONE epoch.
+
+        Should loop over the batches in the dataloader. (Recall the exercise session!)
+        Don't forget to set your model to training mode, i.e., self.model.train()!
+
+        Arguments:
+            dataloader (DataLoader): dataloader for training data
+        """
+        ##
+        ###
+        #### WRITE YOUR CODE HERE!
+        ###
+        ##
+        self.model.train()
+        for batch, (x, y) in enumerate(dataloader):
+            # Compute prediction and loss
+            pred = self.model(x)
+            loss = self.criterion(pred, y)
+
+            # Backpropagation
+            loss.backward()
+            self.optimizer.step()
+            self.optimizer.zero_grad()
+
+    def predict_torch(self, dataloader):
+        """
+        Predict the validation/test dataloader labels using the model.
+
+        Hints:
+            1. Don't forget to set your model to eval mode, i.e., self.model.eval()!
+            2. You can use torch.no_grad() to turn off gradient computation, 
+            which can save memory and speed up computation. Simply write:
+                with torch.no_grad():
+                    # Write your code here.
+
+        Arguments:
+            dataloader (DataLoader): dataloader for validation/test data
+        Returns:
+            pred_labels (torch.tensor): predicted labels of shape (N,),
+                with N the number of data points in the validation/test data.
+        """
+        ##
+        ###
+        #### WRITE YOUR CODE HERE!
+        ###
+        ##
+        pred = []
+        self.model.eval()
+        with torch.no_grad():
+            for x in dataloader:
+                _, label = torch.max(self.model(x[0]), 1)
+                pred.extend(label.tolist())
+        pred_labels = torch.tensor(pred)
+        return pred_labels
+    
+    def fit(self, training_data, training_labels):
+        """
+        Trains the model, returns predicted labels for training data.
+
+        This serves as an interface between numpy and pytorch.
+
+        Arguments:
+            training_data (array): training data of shape (N,D)
+            training_labels (array): regression target of shape (N,)
+        Returns:
+            pred_labels (array): target of shape (N,)
+        """
+
+        # First, prepare data for pytorch
+        train_dataset = TensorDataset(torch.from_numpy(training_data).float(), 
+                                      torch.from_numpy(training_labels).long())
+        train_dataloader = DataLoader(train_dataset, batch_size=self.batch_size, shuffle=True)
+        
+        self.train_all(train_dataloader)
+
+        return self.predict(training_data)
+
+    def predict(self, test_data):
+        """
+        Runs prediction on the test data.
+
+        This serves as an interface between numpy and pytorch.
+        
+        Arguments:
+            test_data (array): test data of shape (N,D)
+        Returns:
+            pred_labels (array): labels of shape (N,)
+        """
+        # First, prepare data for pytorch
+        test_dataset = TensorDataset(torch.from_numpy(test_data).float())
+        test_dataloader = DataLoader(test_dataset, batch_size=self.batch_size, shuffle=False)
+
+        pred_labels = self.predict_torch(test_dataloader)
+
+        # We return the labels after transforming them into numpy array.
+        return pred_labels.cpu().numpy()
